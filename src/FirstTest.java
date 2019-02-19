42ab95b989298cef347e6699534898ee6c85e2d6
Saige Liu <saige.liu@madoor.com>
2019-02-19 10:21:06 -0800

    add library

---
+++ src/FirstTest.java
@@ -0,0 +1,34 @@
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+  @Test
+  public void aftersignin() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void captureScreenShot() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void homepage() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void main() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void profile() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void ustarprofile() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
